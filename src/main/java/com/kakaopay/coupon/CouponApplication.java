b469cb53f9daea5b0f1062c999b7e1bc3fc6a936
junshock5 <61732452+junshock5@users.noreply.github.com>
2020-11-21 01:16:50 +0900

    project create, spring-boot, maven, java8

---
+++ src/main/java/com/kakaopay/coupon/CouponApplication.java
@@ -0,0 +1,13 @@
+package com.kakaopay.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponApplication.class, args);
+	}
+
+}
